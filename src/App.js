500b700e3d84d216127edf5ba7acde6f07fc486f
Ana <anacarolina.gerhardt@hotmail.com>
2019-10-09 02:40:33 -0300

    started adding form and table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="flex-row">
+        <form>
+          <h2>My "To Do" List</h2>
+          <label>Add a new item</label>
+          <input type="text"></input>
+          <button className="muted-button">Done!</button>
+        </form>
+      </div>
+
     </div>
   );
 }
---
+++ src/Tables/ListTable.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+<div className="flex-row margin-top">
+    <table className="striped-table">
+        <thead>
+            <tr>
+            <th>List</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+             <td>Wash clothes</td>
+            </tr>
+            <tr>
+             <td>Bring pets to the vet</td>
+            </tr>
+            <tr>
+              <td>Deposit money</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
