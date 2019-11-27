dae242fb6a02a5c52c99c437fc6ef09e95f85af7
Ana <anacarolina.gerhardt@hotmail.com>
2019-11-27 00:03:54 -0300

    Add build files

---
+++ build/precache-manifest.8c128c363e1b2094d0e5e3081ac02e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d7bff808c81446e38c7f9b675f53e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb0aac34e4726eebe9d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9b53eaef4eb74c70fa40",
+    "url": "/static/css/main.18e102f3.chunk.css"
+  },
+  {
+    "revision": "afb0aac34e4726eebe9d",
+    "url": "/static/js/2.fc09cab9.chunk.js"
+  },
+  {
+    "revision": "9b53eaef4eb74c70fa40",
+    "url": "/static/js/main.3bea48e1.chunk.js"
+  },
+  {
+    "revision": "92dcd62a339e58ab19ee",
+    "url": "/static/js/runtime-main.44f53918.js"
+  }
+]);
